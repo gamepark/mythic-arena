986d16f0c61f83128845261f186a70cfe360dff3
Patrick Beaume <p.beaume@gmail.com>
2025-03-04 14:06:17 +0100

    fix delete animation

--- app/src/animations/GameAnimations.ts
+++ app/src/animations/GameAnimations.ts
@@ -1,10 +1,4 @@
-import { MaterialType } from '@gamepark/mythic-arena/material/MaterialType'
 import { MaterialGameAnimations } from '@gamepark/react-game'
-import { isDeleteItemType } from '@gamepark/rules-api'
 
 export const gameAnimations = new MaterialGameAnimations()
 
-gameAnimations
-  .when()
-  .move(isDeleteItemType(MaterialType.PantheonCard))
-  .none()
