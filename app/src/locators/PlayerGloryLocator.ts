d64d16a8380e4202ebf0c423c079249803360314
Patrick Beaume <p.beaume@gmail.com>
2025-03-21 15:18:03 +0100

    reduce glory point image

--- app/src/locators/PlayerGloryLocator.ts
+++ app/src/locators/PlayerGloryLocator.ts
@@ -5,11 +5,11 @@ import { pantheonCardDescription } from '../material/PantheonCardDescription'
 import { playerHandLocator } from './PlayerHandLocator'
 
 export class PlayerGloryLocator extends ListLocator {
-  gap = { x: gloryDescription.width - 0.3 }
+  gap = { x: gloryDescription.width + 0.3 }
 
   getCoordinates(location: Location, context: MaterialContext) {
     const coordinates = playerHandLocator.getCoordinates(location, context)
-    coordinates.x += pantheonCardDescription.width / 2 - 9.3
+    coordinates.x += pantheonCardDescription.width / 2 - 9
     coordinates.y -= pantheonCardDescription.height * 2
     return coordinates
   }
--- app/src/material/GloryDescription.ts
+++ app/src/material/GloryDescription.ts
@@ -5,7 +5,7 @@ import { GloryTokenHelp } from './help/GloryTokenHelp'
 
 export class GloryDescription extends TokenDescription {
   image = Glory
-  height = 3.013
+  height = 3.35
   width = 2.2
   staticItem = { quantity: 15, location: gloryStockLocation, id: 1 }
   stockLocation = gloryStockLocation
